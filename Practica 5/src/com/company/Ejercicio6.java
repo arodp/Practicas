0695f2cca72cdd19752e018041e20b535756d5e9
Alan Rodriguez <alanrp9662@gmail.com>
2020-09-19 16:26:04 -0400

    Practicas de curso de Java

---
+++ Practica 5/src/com/company/Ejercicio6.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        int[] A = {2,4,6};
+        int[] B = {1,3,5};
+        int[] producto_escalar = new int[1];
+
+        producto_escalar[0] = (A[0] * B[0]) + (A[1] * B[1]) + (A[2] * B[2]);
+        System.out.println(producto_escalar[0]);
+
+
+
+    }
+}
