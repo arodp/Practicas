e93d857815eebdb32ddc404d002a8c470bff9229
Alan Rodriguez <alanrp9662@gmail.com>
2020-09-19 16:27:28 -0400

    Practicas de curso de Java

--- Practica 5/src/com/company/Ejercicio5.java
+++ Practica 5/src/com/company/Ejercicio5.java
@@ -6,8 +6,6 @@ public class Ejercicio5 {
         int[] B = {1,3,5};
         int[] prodcuto_vec = new int[A.length];
 
-
-
         for (int i = 0; i < A.length; i++){
             prodcuto_vec[0] = (A[1] * B[2]) - (A[2] * B[1]);
             prodcuto_vec[1] = -((A[0] * B[2]) - (A[2] * B[0]));
@@ -15,15 +13,5 @@ public class Ejercicio5 {
 
             System.out.println(prodcuto_vec[i]);
         }
-
-
-
-        /*int[][] A_B = new int[2][3];
-        A_B[0][0] = 2;
-        A_B[0][1] = 4;
-        A_B[0][2] = 6;
-        A_B[1][0] = 1;
-        A_B[1][1] = 3;
-        A_B[1][2] = 5;*/
     }
 }
